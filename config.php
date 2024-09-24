027441565fe29108a831a7aea2e699b9e5559697
NAME <mail@gmail.com>
2024-09-24 01:56:54 -0300

    remove vendor

--- config.php
+++ config.php
@@ -5,10 +5,6 @@ define('SERVER_URL', 'http://localhost:8000');
 
 define('CLIENT_URL', 'http://localhost:8080');
 
-<<<<<<< HEAD
-define('SECRET_JWT', 'mujarakrikokulotutiene');
-=======
 define('SECRET_JWT', 'SaDrig.3');
->>>>>>> b33947002c818bf8290df69fb474fabdd557e88c
 
 ?>
